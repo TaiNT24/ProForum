7db2ebb10fbe428678b0d4a820b0d0e75f81f5a5
nguyentutai12@gmail.com <nguyentutai12@gmail.com>
2020-03-13 14:58:24 +0700

    first

---
+++ WebApplication1/Models/Article.cs
@@ -0,0 +1,56 @@
+namespace WebApplication1.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+    using System.Data.SqlClient;
+    using System.Linq;
+
+    [Table("Article")]
+    public partial class Article
+    {
+        [Key]
+        public int ArtID { get; set; }
+
+        [Required]
+        [StringLength(300)]
+        public string ArtTittle { get; set; }
+
+        [Required]
+        [StringLength(3000)]
+        public string ArtContent { get; set; }
+
+        public DateTime ArtPostTime { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string ArtAuthor { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string ArtStatus { get; set; }
+
+        public int ViewCount { get; set; }
+
+        public int LikeCount { get; set; }
+    }
+
+    public class ArticleList
+    {
+        ArticleDBContext context = null;
+
+        public ArticleList()
+        {
+            context = new ArticleDBContext();
+        }
+
+        public List<Article> getListActiveArticle()
+        {
+            String sqlQuery = "select * from Article where ArtStatus='Active'";
+            var list = context.Database.SqlQuery<Article>(sqlQuery).ToList();
+            return list;
+        }
+    }
+}
---
+++ WebApplication1/Models/ArticleDBContext.cs
@@ -0,0 +1,37 @@
+namespace WebApplication1.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class ArticleDBContext : DbContext
+    {
+        public ArticleDBContext()
+            : base("name=ArticleDBContext")
+        {
+        }
+
+        public virtual DbSet<Article> Articles { get; set; }
+        
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Article>()
+                .Property(e => e.ArtTittle)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Article>()
+                .Property(e => e.ArtContent)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Article>()
+                .Property(e => e.ArtAuthor)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Article>()
+                .Property(e => e.ArtStatus)
+                .IsUnicode(false);
+        }
+    }
+}
